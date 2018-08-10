61efe5ed3ed5edfe2122110b360a8859c99d6238
Fausto Silva <fausto.silva@icp.local>
2018-08-10 16:15:53 +0200

    mas cambios, quito comentarios

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  private d = new Date();
+  public year = this.d.getFullYear();
+
   constructor() { }
 
   ngOnInit() {
